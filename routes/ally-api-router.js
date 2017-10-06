a81e781216cdb18cfcb3b7ca8d0f0f87833b1fd1
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-06 16:37:36 -0400

    attempting to add allies  - still not working

--- routes/ally-api-router.js
+++ routes/ally-api-router.js
@@ -9,19 +9,46 @@ router.post('/users/:id/allies', (req, res, user) => {
       res.status(401).json({ errorMessage: 'You must log in to add an ally' });
   }
 
-  const theUser = new UserModel({
-    allies: req.user._id
-  });
-
-  theUser.save((err) => {
-    if(theUser.errors) {
+  req.user.allies.push(theAlly._id);
+  theAlly.save((err) => {
+    if(theAlly.errors){
       res.status(400).json({
-        errorMessage: 'Ally validation failed',
-        validationErrors: theUser.errors
+        errorMessage: 'Validating allies failed',
+        validationErrors: theAlly.errors
       });
       return;
     }
+
+    if(err) {
+      res.status(500).json({ errorMessage: 'New ally went wrong' });
+    }
+
+    UserModel.findById(
+      req.params.allyId,
+      (err, allyFromDb) => {
+        if(err) {
+          res.status(500).json({errorMessage: 'Phone details went wrong 💩'});
+
+        }
+
+        UserModel.findById(
+
+
+        );
+      }
+    );
   });
+
+});
+
+
+router.get('/user/:id/allies', (req, res, next) => {
+
+});
+
+
+
+router.get('/user/:id/allies/:id', (req, res, next) => {
 
 });
 
@@ -30,6 +57,30 @@ router.post('/users/:id/allies', (req, res, user) => {
 
 
 
+//
+// UserModel.findById(
+//   req.params.allyId,
+//
+//   (err, allyFromDb) => {
+//     if(err) {
+//       res.status(500).json({errorMessage: 'allies went wrong??'});
+//     }
+//     return;
+//   }
+// );
+// const theUser = new UserModel({
+//   allies: req.user._id
+// });
+//
+// theUser.save((err) => {
+//   if(theUser.errors) {
+//     res.status(400).json({
+//       errorMessage: 'Ally validation failed',
+//       validationErrors: theUser.errors
+//     });
+//     return;
+//   }
+// });
 
 
 
--- routes/product-api-router.js
+++ routes/product-api-router.js
@@ -10,4 +10,5 @@ const router = express.Router();
 
 
 
+
 module.exports = router;
