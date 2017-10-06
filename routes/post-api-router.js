51e6b7abb7d11055971038e8ffb43bd26ca26678
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-06 10:11:50 -0400

    created get route to retrieve your posts only

--- routes/post-api-router.js
+++ routes/post-api-router.js
@@ -34,10 +34,41 @@ router.post('/posts', (req, res, next) => {
     res.status(200).json(thePost);
     console.log('Succesfully posted!');
 
-  });
+  });//CLOSE thePost.SAVE(...)
+});//CLOSE "router.POST(...)"
+
+//This router gets ALL of the posts vvv
+router.get('/posts', (req, res, next) => {
+    PostModel.find()
+    .sort({ timestamps: -1 })
+    .exec((err, recentPosts) => {
+      if(err) {
+        console.log('error finding posts');
+        res.status(500).json({ errorMessage: 'There was an issue finding posts'});
+        return;
+      }
+      console.log('recentPosts -->');
+      res.status(200).json(recentPosts);
+    });//CLOSE PostModel.find(...)
+});//CLOSE "router.GET(...)"
+
+
+router.get('/myposts', (req, res, next) => {
+  if(!req.user){
+    res.status(401).json({ errorMessage: 'User is not logged in' });
+    return;
+  }
+
+  PostModel.find({ author: req.user._id })
+    .sort({ _id: -1})
+    .exec((err, myPostResults) => {
+          if(err){
+             res.status(500).json({ errorMessage: 'Something went wrong retrieving your posts'}
+           );
+           return;
+          }
+          res.status(200).json(myPostResults);
+    });
 });
-
-
-
 
 module.exports = router;
